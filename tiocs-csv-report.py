3e080942b80f30e809b370e0fc35ac83dd115aa5
jamessmith <jamessmith>
2019-10-04 16:13:27 -0400

    Updated for Python3 and tested

--- tiocs-csv-report.py
+++ tiocs-csv-report.py
@@ -10,121 +10,94 @@
 # ./tiocs-csv-report.py 
 #
 
-
-
 import json
 import os
 import csv
-import sys
-from tenable_io.api.models import Folder
-from tenable_io.client import TenableIOClient
-from tenable_io.exceptions import TenableIOApiException
-from tenable_io.api.models import AssetList, AssetInfo, VulnerabilityList, VulnerabilityOutputList
+import argparse
+from tenable.io import TenableIO
 
-def GenerateReport(accesskey,secretkey,repo):
-	DEBUG=False
+def GenerateReport(DEBUG,accesskey,secretkey,repo, image, tag):
 
-	client = TenableIOClient(access_key=accesskey, secret_key=secretkey)
+	client = TenableIO(accesskey, secretkey)
 
 	#Gather the list of repositories
-	resp=client.get("container-security/api/v1/container/list")
+	resp=client.get("container-security/api/v2/reports/"+repo+"/"+image+"/"+tag)
 	respdata=json.loads(resp.text)
 	if DEBUG:
-		print "Response",respdata
-		print "\n\n"
-	for i in respdata:
-		if DEBUG:
-			print "Repo info:",i
-		if i['name'] == repo:
-			if DEBUG:
-				print "Found repo ID:",i['id']
-			containerid=i['id']
+		print("Response",respdata)
+		print("\n\n")
 
-	#Gather the report for the specified repository
-	resp=client.get("container-security/api/v1/reports/show?container_id="+str(containerid))
-	respdata=json.loads(resp.text)
-	if DEBUG:
-		print "Response",respdata
-		print "\n\n"
+	DEBUG=False
 
 	with open("tiocs-report.csv","w") as csvfile:
-        	fieldnames=['cve','severity','vuln publication date','affected packages','remediation','description']
-        	writer=csv.DictWriter(csvfile,fieldnames=fieldnames)
-       		writer.writeheader()
-		DEBUG=True
-		for i in respdata['findings']:
-			packages=""
-			for j in i['packages']:
-				if packages != "":
-					packages+="\n"
-				if DEBUG:
-					print "Software packages affected",j['name'],j['version']
-				packages=packages+' '+j['name'].encode('utf-8').strip()+' '+j['version'].encode('utf-8').strip()
-			if DEBUG:
-				print i
-				print "\n\n"
-				print "Package",i['packages']
-				print "NVD Finding",i['nvdFinding']
-				print "CVE",i['nvdFinding']['cve']
-				print "Severity",i['nvdFinding']['cvss_score']
-				print "Remediation",i['nvdFinding']['remediation']
-				print "Description",i['nvdFinding']['description']
-				print "Vulnerability publication date",i['nvdFinding']['published_date']
-	
-       		         	rowdict={'cve':i['nvdFinding']['cve'], 'severity': i['nvdFinding']['cvss_score'], 'vuln publication date': i['nvdFinding']['published_date'],'remediation': i['nvdFinding']['remediation'].encode('utf-8').strip(), 'description': i['nvdFinding']['description'].encode('utf-8').strip(), 'affected packages': packages}
-                		writer.writerow(rowdict)
+			fieldnames=['cve','severity','vuln publication date','affected packages','remediation','description']
+			writer=csv.DictWriter(csvfile,fieldnames=fieldnames)
+			writer.writeheader()
+			DEBUG=True
+			for i in respdata['findings']:
+				packages=""
+				for j in i['packages']:
+					if packages != "":
+						packages+="\n"
+					if DEBUG:
+						print("Software packages affected",j['name'],j['version'])
+					packages=packages+' '+str(j['name'])+' '+str(j['version'])
+					if DEBUG:
+						print(i)
+						print("\n\n")
+						print("Package",i['packages'])
+						print("NVD Finding",i['nvdFinding'])
+						print("CVE",i['nvdFinding']['cve'])
+						print("Severity",i['nvdFinding']['cvss_score'])
+						print("Remediation",i['nvdFinding']['remediation'])
+						print("Description",i['nvdFinding']['description'])
+						print("Vulnerability publication date",i['nvdFinding']['published_date'])
 
+					rowdict={'cve':i['nvdFinding']['cve'], 'severity': i['nvdFinding']['cvss_score'], 'vuln publication date': i['nvdFinding']['published_date'],'remediation': str(i['nvdFinding']['remediation']), 'description': str(i['nvdFinding']['description']), 'affected packages': packages}
+					writer.writerow(rowdict)
 	csvfile.close()
-		
-
 	return
 
 ################################################################
 # Start of program 
 ################################################################
-#Set debugging on or off
-DEBUG=True
+parser = argparse.ArgumentParser(description="Pulls a JSON report from Tenable.io CS for a given repository, image name, and tag")
+parser.add_argument('--debug',help="Display a **LOT** of information",action="store_true")
+parser.add_argument('--accesskey',help="The Tenable.io access key",nargs=1,action="store",default=[None])
+parser.add_argument('--secretkey',help="The Tenable.io secret key",nargs=1,action="store",default=[None])
+parser.add_argument('--repo',help="The Tenable.io CS repository",nargs=1,action="store",default=[None])
+parser.add_argument('--image',help="The Tenable.io CS image",nargs=1,action="store",default=[None])
+parser.add_argument('--tag',help="The Tenable.io CS tag",nargs=1,action="store",default=["latest"])
+args = parser.parse_args()
 
+DEBUG=False
+#Set debugging on or off
+
+if args.debug:
+	DEBUG=True
+
+accesskey=None
+secretkey=None
+repo=None
 #Pull as much information from the environment variables
 # as possible, and where missing then initialize the variables.
-if os.getenv('TIOACCESSKEY') is None:
-        accesskey=""
+if args.accesskey[0] is None:
+		accesskey=os.getenv('TIOACCESSKEY')
 else:
-        accesskey=os.getenv('TIOACCESSKEY')
+		accesskey=args.accesskey[0]
 
-if os.getenv('TIOSECRETKEY') is None:
-        secretkey=""
+if args.secretkey[0] is None:
+		secretkey=os.getenv('TIOSECRETKEY')
 else:
-        secretkey=os.getenv('TIOSECRETKEY')
+		secretkey=args.secretkey[0]
 
-if os.getenv('TIOREPOSITORY') is None:
-        repo=""
-else:
-        repo=os.getenv('TIOREPOSITORY')
+if args.repo[0] is not None:
+	repo=args.repo[0]
 
 if DEBUG:
-        print "Connecting to cloud.tenable.com with access key",accesskey,"to report on repository",repo
+	print("Connecting to cloud.tenable.com with access key",accesskey,"to report on repository",repo)
 
-#Pull information from command line.  If nothing there,
-# and there was nothing in the environment variables, then ask user.
-if len(sys.argv) > 1:
-        accesskey=sys.argv[1]
-else:
-        if accesskey == "":
-                accesskey=raw_input("Access key:")
 
-if len(sys.argv) > 2:
-        repo=sys.argv[2]
-else:
-        if repo == "":
-                repo=raw_input("Repository:")
-
-if len(sys.argv) > 3:
-	secretkey=sys.argv[3]
-else:
-	if secretkey == "":
-        	secretkey=raw_input("Secret key:")
-
-GenerateReport(accesskey,secretkey,repo)
+GenerateReport(DEBUG,accesskey,secretkey,repo, args.image[0], args.tag[0])
 
 
